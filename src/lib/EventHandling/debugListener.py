3ea598abfcdecf0435857c68dad124f7f5f19511
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2024-06-03 21:55:48 +0200

    add color ✨

--- src/lib/EventHandling/debugListener.py
+++ src/lib/EventHandling/debugListener.py
@@ -1,6 +1,6 @@
 # debug listener, prints sender and params
 def debug_listener(sender, **kwargs):
-    print(f"[DEBUG] '{sender}' sent data '{", ".join([
+    print(f"\033[33m[DEBUG]\033[0m '{sender}' sent data '{", ".join([
             f"{key} => {value}" for key, value in kwargs.items()
         ])}'"
     )
