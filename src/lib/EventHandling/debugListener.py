2ce2bcebd6a4d5059be4c1b48ad4e92ae67724bd
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2024-06-03 22:36:25 +0200

    make the debug print prettier ✨

--- src/lib/EventHandling/debugListener.py
+++ src/lib/EventHandling/debugListener.py
@@ -1,6 +1,6 @@
 # debug listener, prints sender and params
 def debug_listener(sender, **kwargs):
-    print(f"\033[33m[DEBUG]\033[0m '{sender}' sent data '{", ".join([
+    print(f"\033[33m[DEBUG]\033[0m '{sender}' sent event \n\t{{\n\t\t{",\n\t\t".join([
             f"{key} => {value}" for key, value in kwargs.items()
-        ])}'"
+        ])} \n\t}}"
     )
